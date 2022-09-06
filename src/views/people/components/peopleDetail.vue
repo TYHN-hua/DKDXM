0a8b5af80c9bf5eaf56a5cb5c32dde5d30e222f9
limu <1259567994@qq.com>
2022-09-06 19:35:19 +0800

    1

--- src/views/people/components/peopleDetail.vue
+++ src/views/people/components/peopleDetail.vue
@@ -8,13 +8,11 @@
     <div class="descriptions">
       <span>
         <el-descriptions style="padding-left: 10px">
-          <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-          <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-          <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-          <el-descriptions-item label="备注">
-            <el-tag size="small">学校</el-tag>
-          </el-descriptions-item>
-          <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+          <el-descriptions-item label="人员名称">{{ userInfo.userName }}</el-descriptions-item>
+          <el-descriptions-item label="角色">{{ userInfo.roleName }}</el-descriptions-item>
+          <el-descriptions-item label="联系电话">{{ userInfo.mobile }}</el-descriptions-item>
+
+          <el-descriptions-item label="负责区域">{{ userInfo.regionName }}</el-descriptions-item>
         </el-descriptions>
       </span>
     </div>
@@ -53,7 +51,7 @@ export default {
 
   .el-descriptions__table{
     background-color: #f3f6fb;
-    padding-left: 20px;
+    padding: 20px;
   }
 }
 </style>
